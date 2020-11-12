ec85021be557cae760343f56822af2825ca1be2c
Jakub Pusiak <jakub.pusiak@gmail.com>
2020-11-12 23:48:45 +0100

    Install react and bulma.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World')
+import React from 'react';
+import { render } from 'react-dom';
+import '../node_modules/bulma/css/bulma.min.css';
+
+render(
+  <p>Hello World</p>,
+  document.getElementById('root'),
+);
