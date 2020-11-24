ab4cac14283add842e621bbefa584abf0bd1f05f
Jakub Pusiak <jakub.pusiak@gmail.com>
2020-11-25 00:21:54 +0100

    Revert "Refactor code 1"

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import StatePage from './StatePage';
+import React from 'react';
 import StateSearch from './StateSearch';
-import { searchStates, getState } from '../services/state';
-import Select from './Select';
 
 const App = () => {
-  const [query, setQuery] = useState('');
-  const [resultsList, setResultsList] = useState([]);
-  const [pickedStateCode, setPickedStateCode] = useState(null);
-  const [searchKey, setSearchKey] = useState('state');
-  const [dataType, setDataType] = useState('table');
-  const currentState = getState(pickedStateCode);
-
-  useEffect(() => {
-    setResultsList(searchStates(query, searchKey));
-  }, [query]);
-
   return (
     <div className="App">
-      <Select
-        label="Wyszukuj po"
-        onSelect={setSearchKey}
-        value={searchKey}
-      >
-        <option value="state">nazwa</option>
-        <option value="code">kod stanu</option>
-        <option value="capital_city">stolica</option>
-      </Select>
-      <StateSearch
-        query={query}
-        onSetQuery={setQuery}
-        list={resultsList}
-        onPick={({ code }) => setPickedStateCode(code)}
-      />
-      <Select
-        label="Pokaż dane"
-        onSelect={setDataType}
-        value={dataType}
-      >
-        <option value="table">tabela</option>
-        <option value="symbols">symbole</option>
-        <option value="map">mapa</option>
-      </Select>
-      {currentState && (
-        <StatePage 
-          state={currentState}
-          dataType={dataType}
-        />
-      )}
+      <div className="field">
+        <label className="label">Wyszukuj po</label>
+        <div className="control">
+          <div className="select">
+            <select>
+              <option>nazwa</option>
+              <option>kod stanu</option>
+              <option>stolica</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <StateSearch />
+      <div className="field">
+        <label className="label">Pokaż dane</label>
+        <div className="control">
+          <div className="select">
+            <select>
+              <option>tabela</option>
+              <option>symbole</option>
+              <option>mapa</option>
+            </select>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
