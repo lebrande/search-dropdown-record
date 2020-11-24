99d03a11174031f0ae1093a7bb511bb35b10268b
Jakub Pusiak <jakub.pusiak@gmail.com>
2020-11-24 23:49:31 +0100

    Refactor code.

--- src/components/StatePage.js
+++ src/components/StatePage.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
 const StatePage = ({
-  onBack,
   state: {
     state,
-    nickname,
     admission_date,
     capital_city,
     code,
@@ -14,53 +12,51 @@ const StatePage = ({
     state_flag_url,
     state_seal_url,
   },
+  dataType,
 }) => {
   return (
     <div className="StatePage content">
-      <button
-        className="button"
-        onClick={onBack}
-      >
-        Wróć do wyszukiwania
-      </button>
       <h1 className="title is-1">{state}</h1>
-      <p className="title is-3">{nickname}</p>
 
-      <div className="Symbols">
-        <div className="ImageWrapper">
-          <img className="Flag" src={state_flag_url} />
+      {dataType === 'symbols' && (
+        <div className="Symbols">
+          <div className="ImageWrapper">
+            <img className="Flag" src={state_flag_url} />
+          </div>
+          <div className="ImageWrapper">
+            <img src={state_seal_url} />
+          </div>
         </div>
-        <div className="ImageWrapper">
-          <img src={state_seal_url} />
-        </div>
-      </div>
+      )}
 
-      <table className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-        <tbody>
-          <tr>
-            <td>Data przyłączenia</td>
-            <td>{admission_date}</td>
-          </tr>
-          <tr>
-            <td>Stolica</td>
-            <td>{capital_city}</td>
-          </tr>
-          <tr>
-            <td>Kod</td>
-            <td>{code}</td>
-          </tr>
-          <tr>
-            <td>Mieszkańcy</td>
-            <td>{population}</td>
-          </tr>
-          <tr>
-            <td>Ranking mieszkańców</td>
-            <td>{population_rank}</td>
-          </tr>
-        </tbody>
-      </table>
+      {dataType === 'table' && (
+        <table className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+          <tbody>
+            <tr>
+              <td>Data przyłączenia</td>
+              <td>{admission_date}</td>
+            </tr>
+            <tr>
+              <td>Stolica</td>
+              <td>{capital_city}</td>
+            </tr>
+            <tr>
+              <td>Kod</td>
+              <td>{code}</td>
+            </tr>
+            <tr>
+              <td>Mieszkańcy</td>
+              <td>{population}</td>
+            </tr>
+            <tr>
+              <td>Ranking mieszkańców</td>
+              <td>{population_rank}</td>
+            </tr>
+          </tbody>
+        </table>
+      )}
 
-      <img className="Map" src={map_image_url} />
+      {dataType === 'map' && <img className="Map" src={map_image_url} />}
     </div>
   );
 };
---
+++ src/services/state.js
@@ -0,0 +1,17 @@
+import states from './states.json';
+
+export const searchStates = (query, searchKey) => {
+  const lowerCaseQuery = query.toLowerCase()
+
+  return states
+    .filter((state) => {
+      return state[searchKey]
+        .toLowerCase()
+        .startsWith(lowerCaseQuery);
+    })
+    .slice(0, 6)
+};
+
+export const getState = (codeToFind) => {
+  return states.find(({ code }) => code === codeToFind)
+}
